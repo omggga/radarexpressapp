259c6aa2ce587a0de9bff6f5ff17c3f75387c950
Alexander <omggga@gmail.com>
2019-04-03 12:21:10 +0300

    connecting to remote db

--- index.js
+++ index.js
@@ -2,18 +2,20 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-const pgp = require('pg-promise')
-const pgsql = 'postgres://qvduspzt:nqQSJHteBSE1_APAMeQHFP6h7FhuGipl@balarama.db.elephantsql.com:5432/qvduspzt'
-const db = pgp(pgsql)
+const pgp = require('pg-promise')()
+const connection = 'postgres://qvduspzt:nqQSJHteBSE1_APAMeQHFP6h7FhuGipl@balarama.db.elephantsql.com:5432/qvduspzt'
+//https://api.elephantsql.com/console/2902d413-c14f-452e-be8f-1a29d4ffaf12/details?
+
 
 app.get('/', (req, res) =>  {
 
-	try {
-		const users = yield db.any('CREATE TABLE users (user_id bigserial primary key, user_key varchar(20) NOT NULL, from text NOT NULL, to text NOT NULL, visa text default NULL, dates text NOT NULL, price text NOT NULL, date_added timestamp default NULL);', [true])
-	} catch(e) {
-		console.log(e)
-	}
-
+	const db = pgp(connection)
+	db.any('SELECT * FROM account', [true]).then(function(data) {
+		console.log('data', data)
+	})
+	.catch(function(error) {
+		console.log('error', error)
+	})
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
