9036f28d44e16ff885b2dcc56ae5f70925f0a0b6
omggga <omggga@gmail.com>
2019-05-13 22:55:27 +0300

    wip

--- src/vue/CompleteComponent.vue
+++ src/vue/CompleteComponent.vue
@@ -1,10 +1,10 @@
 <template lang="pug">
 	v-layout.pt-1.pb-1(wrap, justify-start)
 		v-flex(xs1)
-			img(src="static/images/logo.jpg", alt="Travel Radar", width="70", height="70")
+			img(src="static/images/cropped-trlogo_002.png", alt="Travel Radar", width="67", height="68")
 		v-flex.pt-4(xs11)
 			span.headline.pl-2 Теперь лети отсюда!
 		v-flex.pt-4.pb-4(xs12)
-			p Данные успешно сохранены!
-			p Теперь ты подписан на оповещения по заданному тобой фильтру. А так же в любой момент можешь изменить или удалить подписку через бота.
+			p Мы запомнили твой выбор и он неплох.
+			p Теперь все новости будут фильтроваться только под твои запросы. Ты можешь поменять фильтры в любое время, для этого отправь боту новое сообщение РАДАРПОМОГИ.
 </template>
